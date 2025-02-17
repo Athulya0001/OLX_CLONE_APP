6dfd0cea91462c22f44f97286649cd7abc76eee5
athu <athulyas2024@gmail.com>
2025-02-17 22:38:48 +0530

    user authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Routes, Route} from 'react-router-dom'
+import Signin from './components/Signin/Signin'
+import Signup from './components/Signup/Signup'
+import Home from './pages/Home/Home'
+
 
 const App = () => {
   return (
     <div>
-      App
-    </div>
+      <Routes>
+        <Route path="/" element={<Signin />} /> 
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>    </div>
   )
 }
 
